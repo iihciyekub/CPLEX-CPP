9a8ac8a2131497869bb3aec820c17fb546dc55dd
lyj <lyj8512@126.com>
2020-04-05 09:46:20 +0800

    first commit

---
+++ 02 配置第一个项目/detail.md
@@ -0,0 +1,18 @@
+# concert
+
+````
+C:\Program Files (x86)\IBM\ILOG\CPLEX_Studio1261\concert\include
+
+C:\Program Files (x86)\IBM\ILOG\CPLEX_Studio1261\concert\lib\x86_windows_vs2013\stat_mda
+````
+
+
+
+# CPLEX
+
+````
+C:\Program Files (x86)\IBM\ILOG\CPLEX_Studio1261\cplex\include
+
+C:\Program Files (x86)\IBM\ILOG\CPLEX_Studio1261\cplex\lib\x86_windows_vs2013\stat_mda
+````
+
---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<ilcplex/ilocplex.h>
+ILOSTLBEGIN
+int main()
+{
+	IloEnv env;
+	IloModel model(env);
+	IloNumVar x1(env, 0, 99, ILOINT);
+	IloCplex cpl(model);
+	IloConstraint cont,cont2;
+
+	cont = (x1 * 5 >= 10);
+	cont2 = (x1 >= 3);
+	model.add(IloMinimize(env, x1 * 6));
+	model.add(cont);
+	model.add(cont2);
+	cpl.solve();
+	env.out() << cpl.getValue(x1) << endl << cpl.getObjValue() << endl;
+	system("pause");
+}
